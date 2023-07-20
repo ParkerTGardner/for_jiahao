f7a79f8eeb337fb602805477721d4d7afc575e8f
pgardner <ptg2@rice.edu>
2023-07-20 04:54:37 +0200

    fixing run error

--- HeavyIonAnalysis/TrackAnalysis/batch/pythia_batch_output/pythia_sub.sh
+++ HeavyIonAnalysis/TrackAnalysis/batch/pythia_batch_output/pythia_sub.sh
@@ -1,14 +1,12 @@
-!/bin/sh
+#!/bin/sh
 
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-export SCRAM_ARCH=slc7_amd64_gcc700
+#source /cvmfs/cms.cern.ch/cmsset_default.sh
+#export SCRAM_ARCH=slc7_amd64_gcc700
 
+cd ~/local_not_scp/CMSSW_10_6_4_patch1/src
 
-cd ~/CMSSW_10_6_4_patch1/src
-
-cmsenv
-
+#cmsenv
 eval `scramv1 runtime -sh`
-cd for_jiahao/HeavyIonAnalysis/TrackAnalysis/batch
+cd ~/for_jiahao/HeavyIonAnalysis/TrackAnalysis/batch
 echo PWD: $PWD
 ../bin/PYTHIA_gen.exe ./pythia_lists/list_cor_$1 0 1
