0b219e70aad9a8485a2a05fd498e8e3eaea344b2
Jiahao Yan <jiahaoy@lxplus778.cern.ch>
2023-07-26 04:33:41 +0200

    pythia_sub.sh

--- HeavyIonAnalysis/TrackAnalysis/batch/pythia_batch_output/pythia_sub.sh
+++ HeavyIonAnalysis/TrackAnalysis/batch/pythia_batch_output/pythia_sub.sh
@@ -2,13 +2,12 @@
 
 #source /cvmfs/cms.cern.ch/cmsset_default.sh
 #export SCRAM_ARCH=slc7_amd64_gcc700
-
-cd ~/second_checkout
+cd ~/CMSSW_10_6_4_patch1/src
 
 
 #cmsenv
 
 eval `scramv1 runtime -sh`
-cd ~/for_jiahao/HeavyIonAnalysis/TrackAnalysis/batch
+cd ~/second_checkout/for_jiahao/HeavylonAnalysis/TrackAnalysis/batch/
 echo PWD: $PWD
 ../bin/PYTHIA_gen.exe  ./pythia_lists/list_cor_$1 0 1
