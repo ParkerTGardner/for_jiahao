677bd0c8098f175c740f9dbdb6cd66d4f764b7fe
yanjiahao <jy123@rice.edu>
2023-08-01 17:14:38 -0500

    try with previous data

--- Fourier_analysis.cpp
+++ Fourier_analysis.cpp
@@ -13,12 +13,14 @@
 #include "TKey.h"
 #include "TClass.h"
 #include "TROOT.h"
-#include "include/sherpa_constants.h"
+#include "sherpa_constants.h"
+// TH1::SetDefaultSumw2(kTRUE);
+// TH2::SetDefaultSumw2(kTRUE);
 
-void Fourier_Analysis() {
+void Fourier_analysis() {
     // Open the file
-    TFile* file = new TFile("job_morebins_all.root");
-    std::cout << "Opened file: job_morebins_all.root" << std::endl;
+    TFile* file = new TFile("job_morebins_all_2.root");
+    std::cout << "Opened file: job_morebins_all_2.root" << std::endl;
     // Loop over all objects in the file
     TIter next(file->GetListOfKeys());
     TKey *key;
@@ -39,29 +41,59 @@ void Fourier_Analysis() {
         std::cout << "Extracted parameters: " << wtrk << ", " << wppt << ", "  << wpPU << std::endl;
         // Perform the analysis for this histogram
         // ...
-        TH2D* hSig = (TH2D*)file->Get(Form("hjtw_SignalS_trk_%d_ppt_%d_PU_%d" , wtrk, wppt, wpPU));
-        TH2D* hBack = (TH2D*)file->Get(Form("hjtw_BckrndS_trk_%d_ppt_%d_PU_%d" , wtrk, wppt, wpPU));
-        std::cout << Form("hjtw_SignalS_trk_%d_ppt_%d_PU_%d" , wtrk, wppt, wpPU) << std::endl;
+        
+        // TH2D* hSig = (TH2D*)file->Get(Form("hjtw_SignalS_trk_%d_ppt_%d_PU_%d" , wtrk, wppt, wpPU));
+        // if(!hSig){
+        //     std::cout<< "No sig"<<std::endl;
+        // }
+        // TH2D* hBack = (TH2D*)file->Get(Form("hjtw_BckrndS_trk_%d_ppt_%d_PU_%d" , wtrk, wppt, wpPU));
+        TH2D* hSig = (TH2D*)file->Get(Form("hSigS_%d_to_%d_and_%d_to_%d_w_PU_1", trackbinbounds[wtrk-1],trackbinboundsUpper[wtrk-1], (int)(10*ptbinbounds_lo[wppt-1]), (int)(10*ptbinbounds_hi[wppt-1])));
+        TH2D* hBack = (TH2D*)file->Get(Form("hBckS_%d_to_%d_and_%d_to_%d_w_PU_1", trackbinbounds[wtrk-1],trackbinboundsUpper[wtrk-1], (int)(10*ptbinbounds_lo[wppt-1]), (int)(10*ptbinbounds_hi[wppt-1])));
+        
+        hSig->SetDefaultSumw2(kTRUE);
+        hBack->SetDefaultSumw2(kTRUE);
+        // std::cout << Form("hjtw_SignalS_trk_%d_ppt_%d_PU_%d" , wtrk, wppt, wpPU) << std::endl;
         // Divide the signal histogram by the background histogram
-        hSig->Divide(hBack);
-
-        // Create a 1D projection for DeltaEta > 2
         double DelEta_lo = 2.0;
-        TH1D* h1D = hSig->ProjectionY("h1D", hSig->GetXaxis()->FindBin(DelEta_lo), -1);
+        TH1D *hSig1D = (TH1D*) hSig->ProjectionY("h1D", hSig->GetXaxis()->FindBin(DelEta_lo), -1)->Clone();
+        TH1D *hBack1D = (TH1D*) hBack->ProjectionY("h1D", hBack->GetXaxis()->FindBin(DelEta_lo), -1)->Clone();
+        hSig1D->SetDefaultSumw2(kTRUE);
+        hBack1D->SetDefaultSumw2(kTRUE);
+        hSig1D->Divide(hBack1D);
+        hSig1D->Scale(hBack1D->GetMaximum());
+        TH1D* h1D = hSig1D;
+        h1D->SetDefaultSumw2(kTRUE);
+        // Create a 1D projection for DeltaEta > 2
+        
+        // TH1D* h1D = hSig->ProjectionY("h1D", hSig->GetXaxis()->FindBin(DelEta_lo), -1);
         
         // Define a 3 term harmonic function
-        double normalizationFactor = h1D->GetMaximum();
-        TF1* func = new TF1("func", "[0]/(TMath::Pi()*2)*([1]+2*([2]*TMath::Cos(x)+[3]*TMath::Cos(2*x)+[4]*TMath::Cos(3*x)))", h1D->GetXaxis()->GetXmin(), h1D->GetXaxis()->GetXmax());
-        func->FixParameter(0, normalizationFactor);
-        func->FixParameter(1, 1);
+        std::string function = "[0]/(TMath::Pi()*2)*(1+2*([1]*TMath::Cos(x)+[2]*TMath::Cos(2*x)+[3]*TMath::Cos(3*x)+[4]*TMath::Cos(4*x)))";
+        TF1* func = new TF1("func", function.c_str(), -0.5*TMath::Pi(), 1.5*TMath::Pi());
+        // double normalizationFactor = h1D->GetMaximum();
+        // TF1* func = new TF1("func", "[0]/(TMath::Pi()*2)*([1]+2*([2]*TMath::Cos(x)+[3]*TMath::Cos(2*x)+[4]*TMath::Cos(3*x)))", h1D->GetXaxis()->GetXmin(), h1D->GetXaxis()->GetXmax());
+        // func->FixParameter(0, normalizationFactor);
+        // func->FixParameter(1, 1);
+
+        func->SetParameter(0, h1D->GetMaximum());
+        func->SetParameter(1, 0);
+        func->SetParameter(2, 0);
+        func->SetParameter(3, 0);
+        h1D->Fit(func, "q 0");
+        h1D->Fit(func, "q 0");
+        h1D->Fit(func, "m q 0");
+        h1D->Fit(func, "m q 0");
+        h1D->Fit(func, "m q E 0");
+        h1D->Fit(func, "m E q 0");
+
         // Fit the function to the histogram
-        h1D->Fit(func, "NQ");
+        // h1D->Fit(func, "NQ");
 
         // Get the parameters
         // double v0 = func->GetParameter(0);
-        double v1 = func->GetParameter(2);
-        double v2 = func->GetParameter(3);
-        double v3 = func->GetParameter(4);
+        double v1 = func->GetParameter(1);
+        double v2 = func->GetParameter(2);
+        double v3 = func->GetParameter(3);
 
         
 
@@ -78,25 +110,26 @@ void Fourier_Analysis() {
        
         
 
-        // Normalize func
-        // double scaleParam = func->GetParameter(0);
-        func->SetParameter(0, M_PI);
-        // std::cout << scaleParam / normalizationFactor <<std::endl;
-        // std::cout << func->GetParameter(1) <<std::endl;
+
+        // func->SetParameter(0, M_PI);
+     
         
-        func->SetParameter(1, 0);
-        h1D->Scale( 1 / normalizationFactor);
+        // func->SetParameter(1, 0);
+        // h1D->Scale( 1 / normalizationFactor);
         // Reduce the size of the error bars
-        for (int i = 1; i <= h1D->GetNbinsX(); i++) {
-            double binError = h1D->GetBinError(i);
-            double binContent = h1D->GetBinContent(i);
-            h1D->SetBinContent(i, binContent - 0.5);
-            h1D->SetBinError(i, binError / normalizationFactor *M_PI);
-            }       
+        // for (int i = 1; i <= h1D->GetNbinsX(); i++) {
+        //     double binError = h1D->GetBinError(i);
+        //     double binContent = h1D->GetBinContent(i);
+        //     h1D->SetBinContent(i, binContent - 0.5);
+        //     h1D->SetBinError(i, binError / normalizationFactor *M_PI);
+        //     }       
         
         // Draw the histogram and the fit
         TCanvas* c1 = new TCanvas("c1", "Fourier_fit", 800, 600);
+        TString graphtitle = Form("Fourier_fit_for_track_%d_to_%d_pt_%.2f_to_%.2f.png", trackbinbounds[wtrk-1], trackbinboundsUpper[wtrk-1], ptbinbounds_lo[wppt-1], ptbinbounds_hi[wppt-1]);
         h1D->Draw();
+        h1D->SetTitle(graphtitle);
+        h1D->GetXaxis()->SetTitle("#Delta eta");
         func->SetLineColor(kRed);
         func->Draw("same");
         
@@ -129,7 +162,7 @@ void Fourier_Analysis() {
 
         // Save the canvas
         // c1->SaveAs(Form("Fourier_fit/Fourier_fit_for_pt_%d_to_%d.png", ptbin_lo, ptbin_hi));
-        TString filename = Form("Fourier_fit/Fourier_fit_for_track_%d_to%d_pt_%.2f_to_%.2f.png", trackbinbounds[wtrk-1], trackbinboundsUpper[wtrk-1], ptbinbounds_lo[wppt-1], ptbinbounds_hi[wppt-1]);
+        TString filename = "Fourier_fit/"+ graphtitle;
         std::cout << "Saving canvas to: " << filename << std::endl;
         c1->SaveAs(filename);
         // Clean up
@@ -149,6 +182,6 @@ void Fourier_Analysis() {
 }
 
 int main() {
-    Fourier_Analysis();
+    Fourier_analysis();
     return 0;
 }
--- Fourier_for_each_pt.cpp
+++ Fourier_for_each_pt.cpp
@@ -15,13 +15,13 @@
 #include "TClass.h"
 #include "TROOT.h"
 #include "TGraph.h" 
-#include "include/sherpa_constants.h"
+#include "sherpa_constants.h"
 #include <vector>
 #include <string>
 #include <iostream>
 #include <fstream>
 #include "math.h"
-
+#include "TGraphErrors.h" 
 void processRootFile() {
    
 
@@ -49,32 +49,67 @@ void processRootFile() {
             TCanvas* c1 = new TCanvas("c1", v_for_pt, 800, 600);
 
             // Create a graph for each array
-            TGraph* g1 = new TGraph(trackbin);
-            TGraph* g2 = new TGraph(trackbin);
-            TGraph* g3 = new TGraph(trackbin);
+            TGraphErrors* g1 = new TGraphErrors(trackbin);
+            TGraphErrors* g2 = new TGraphErrors(trackbin);
+            TGraphErrors* g3 = new TGraphErrors(trackbin);
             int point = 0;
         
             for (int wtrk = 1; wtrk < trackbin+1; wtrk++) {
                 // Get the histogram
                 TH2D* hSig = (TH2D*)file->Get(Form("hSigS_%d_to_%d_and_%d_to_%d_w_PU_1", trackbinbounds[wtrk-1],trackbinboundsUpper[wtrk-1], (int)(10*ptbinbounds_lo[wppt-1]), (int)(10*ptbinbounds_hi[wppt-1])));
                 TH2D* hBack = (TH2D*)file->Get(Form("hBckS_%d_to_%d_and_%d_to_%d_w_PU_1", trackbinbounds[wtrk-1],trackbinboundsUpper[wtrk-1], (int)(10*ptbinbounds_lo[wppt-1]), (int)(10*ptbinbounds_hi[wppt-1])));
-                hSig->Divide(hBack);
-                // Create a 1D projection for DeltaEta > 2
+                hSig->SetDefaultSumw2(kTRUE);
+                hBack->SetDefaultSumw2(kTRUE);
+
                 double DelEta_lo = 2.0;
-                TH1D* h1D = hSig->ProjectionY("h1D", hSig->GetXaxis()->FindBin(DelEta_lo), -1);
+                TH1D *hSig1D = (TH1D*) hSig->ProjectionY("h1D", hSig->GetXaxis()->FindBin(DelEta_lo), -1)->Clone();
+                TH1D *hBack1D = (TH1D*) hBack->ProjectionY("h1D", hBack->GetXaxis()->FindBin(DelEta_lo), -1)->Clone();
+                hSig1D->SetDefaultSumw2(kTRUE);
+                hBack1D->SetDefaultSumw2(kTRUE);
+                hSig1D->Divide(hBack1D);
+                hSig1D->Scale(hBack1D->GetMaximum());
+                TH1D* h1D = hSig1D;
+                h1D->SetDefaultSumw2(kTRUE);
                 
-                // Define a 3 term harmonic function
-                double normalizationFactor = h1D->GetMaximum();
-                TF1* func = new TF1("func", "[0]/(TMath::Pi()*2)*(1+2*([1]*TMath::Cos(x)+[2]*TMath::Cos(2*x)+[3]*TMath::Cos(3*x)))", h1D->GetXaxis()->GetXmin(), h1D->GetXaxis()->GetXmax());
-                func->SetParameter(0, normalizationFactor);
+                // hSig->Divide(hBack);
+                // Create a 1D projection for DeltaEta > 2
+                // double DelEta_lo = 2.0;
+                // TH1D* h1D = hSig->ProjectionY("h1D", hSig->GetXaxis()->FindBin(DelEta_lo), -1);
+                
+                // // Define a 3 term harmonic function
+                // double normalizationFactor = h1D->GetMaximum();
+                // TF1* func = new TF1("func", "[0]/(TMath::Pi()*2)*(1+2*([1]*TMath::Cos(x)+[2]*TMath::Cos(2*x)+[3]*TMath::Cos(3*x)))", h1D->GetXaxis()->GetXmin(), h1D->GetXaxis()->GetXmax());
+                // func->SetParameter(0, normalizationFactor);
+
+                std::string function = "[0]/(TMath::Pi()*2)*(1+2*([1]*TMath::Cos(x)+[2]*TMath::Cos(2*x)+[3]*TMath::Cos(3*x)+[4]*TMath::Cos(4*x)))";
+                TF1* func = new TF1("func", function.c_str(), -0.5*TMath::Pi(), 1.5*TMath::Pi());
+                // double normalizationFactor = h1D->GetMaximum();
+                // TF1* func = new TF1("func", "[0]/(TMath::Pi()*2)*([1]+2*([2]*TMath::Cos(x)+[3]*TMath::Cos(2*x)+[4]*TMath::Cos(3*x)))", h1D->GetXaxis()->GetXmin(), h1D->GetXaxis()->GetXmax());
+                // func->FixParameter(0, normalizationFactor);
+                // func->FixParameter(1, 1);
+
+                func->SetParameter(0, h1D->GetMaximum());
+                func->SetParameter(1, 0.1);
+                func->SetParameter(2, 0.1);
+                func->SetParameter(3, 0.1);
+                h1D->Fit(func, "q 0");
+                h1D->Fit(func, "q 0");
+                h1D->Fit(func, "m q 0");
+                h1D->Fit(func, "m q 0");
+                h1D->Fit(func, "m q E 0");
+                h1D->Fit(func, "m E q 0");
+
 
                 // Fit the function to the histogram
-                h1D->Fit(func, "NQ");
+                // h1D->Fit(func, "NQ");
                 // std::cout << "fit" << std::endl;
                 // Get the parameters
                 g1->SetPoint(point, trackbinbounds[wtrk-1], func->GetParameter(1));
+                g1->SetPointError(point, 0, func->GetParError(1));
                 g2->SetPoint(point, trackbinbounds[wtrk-1], func->GetParameter(2));
+                g2->SetPointError(point, 0, func->GetParError(2));
                 g3->SetPoint(point, trackbinbounds[wtrk-1], func->GetParameter(3));
+                g3->SetPointError(point, 0, func->GetParError(3));
                 point++;
                 // std::cout << "draw" << std::endl;
             
@@ -102,11 +137,11 @@ void processRootFile() {
             g1->SetTitle(v_for_pt);
             g1->GetXaxis()->SetTitle("Multiplicity");
             g1->SetLineColor(kBlue);
-            g1->Draw("AL");
+            g1->Draw("ACP");
             g2->SetLineColor(kGreen);
-            g2->Draw("L same");
+            g2->Draw("CP same");
             g3->SetLineColor(kMagenta);
-            g3->Draw("L same");
+            g3->Draw("CP same");
             legend->Draw();
             c1->Update();
 
