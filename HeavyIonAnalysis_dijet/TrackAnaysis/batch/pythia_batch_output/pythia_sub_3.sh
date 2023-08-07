a621ee31d275fd7bceb660c500ade5f0d2abdb37
yanjiahao <jy123@rice.edu>
2023-08-07 10:44:58 -0500

    5th

--- HeavyIonAnalysis_dijet/TrackAnaysis/batch/pythia_batch_output/pythia_sub_3.sh
+++ HeavyIonAnalysis_dijet/TrackAnaysis/batch/pythia_batch_output/pythia_sub_3.sh
@@ -10,4 +10,4 @@ cd ~/CMSSW_10_6_4_patch1/src
 eval `scramv1 runtime -sh`
 cd ~/second_checkout/for_jiahao/HeavyIonAnalysis_dijet/TrackAnalysis/batch/
 echo PWD: $PWD
-../bin/PYTHIA_gen_3.exe  ./pythia_lists/list_cor_$1 0 1
+../bin/PYTHIA_gen_bi_3.exe  ./pythia_lists/list_cor_$1 0 1
