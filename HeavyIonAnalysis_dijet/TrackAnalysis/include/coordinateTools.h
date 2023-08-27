7da27665b165b0354e7fb5979e170e382bc98c1a
yanjiahao <jy123@rice.edu>
2023-08-27 11:23:29 -0500

    modify boost

--- HeavyIonAnalysis_dijet/TrackAnalysis/include/coordinateTools.h
+++ HeavyIonAnalysis_dijet/TrackAnalysis/include/coordinateTools.h
@@ -92,7 +92,8 @@ double BeamBoost( double jetPt, double jetEta, double jetPhi, double trkPt, doub
   // boost with Jet.pz
   TVector3 j0 = TVector3(0,0,0);
   j0.SetPtEtaPhi( jetPt, jetEta, jetPhi);
-  TVector3 j (0,0,j0.Z()/j0.Mag());
+  //the boost func in root is inversed boost, so here should use -v
+  TVector3 j (0,0,-j0.Z()/j0.Mag());
 
   TVector3 trk0 = TVector3(0,0,0);
   trk0.SetPtEtaPhi( trkPt, trkEta, trkPhi);
