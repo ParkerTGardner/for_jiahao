bdf33d27ff825bab577faae2e4d4958085e258a9
yanjiahao <jy123@rice.edu>
2023-09-12 19:00:09 -0500

    Off diagonal

--- OffDiagonalAnalysis/TrackAnalysis/batch/pythia_batch_output/pythia_sub.sh
+++ OffDiagonalAnalysis/TrackAnalysis/batch/pythia_batch_output/pythia_sub.sh
@@ -8,6 +8,6 @@ cd ~/CMSSW_10_6_4_patch1/src
 #cmsenv
 
 eval `scramv1 runtime -sh`
-cd ~/second_checkout/for_jiahao/HeavyIonAnalysis_dijet/TrackAnalysis/batch/
+cd ~/second_checkout/for_jiahao/OffDiagonalAnalysis/TrackAnalysis/batch/
 echo PWD: $PWD
 ../bin/PYTHIA_gen_di.exe  ./pythia_lists/list_cor_$1 0 1
