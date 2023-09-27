9942742e6e1b9234af8377335f3f85e03102d782
yanjiahao <jy123@rice.edu>
2023-09-27 07:49:39 -0500

    more cumulant

--- HeavyIonAnalysis_dijet/TrackAnalysis/src/PYTHIA_gen_di_copy_2.C
+++ HeavyIonAnalysis_dijet/TrackAnalysis/src/PYTHIA_gen_di_copy_2.C
@@ -319,7 +319,7 @@ std::cout << "File is " << fileList.at(f).c_str() << endl;
                         M++;
                     }
 
-                    else if ((jet_dau_eta>3.00) && (jet_dau_eta<5.00)){
+                    else if ((jet_dau_eta>2.25) && (jet_dau_eta<5.00)){
 
                         Q_all2T = Q_all2T + Q_part2;
                         Q_all4T = Q_all4T + Q_part4;
@@ -332,7 +332,7 @@ std::cout << "File is " << fileList.at(f).c_str() << endl;
                     //if((*chargedMultiplicity)[indicesR[kjet]] >= trackbinbounds[i] && (*chargedMultiplicity)[indicesR[kjet]] < trackbinboundsUpper[i]){
                         if(tkBool[i] == 1){
                             if (jet_dau_eta>0.86 && jet_dau_eta<1.75) hPhiDrawA[i]->Fill(phi);
-                            if (jet_dau_eta>3.00 && jet_dau_eta<5.00) hPhiDrawT[i]->Fill(phi);
+                            if (jet_dau_eta>2.25 && jet_dau_eta<5.00) hPhiDrawT[i]->Fill(phi);
                         }
                     }
 
@@ -525,7 +525,7 @@ std::cout << "File is " << fileList.at(f).c_str() << endl;
                         
                     }
 
-                    else if (jet_dau_eta>3.00 && jet_dau_eta<5.00){
+                    else if (jet_dau_eta>2.25 && jet_dau_eta<5.00){
 
                         for(int i = 0; i < trackbin; i++){
                             if(tkBool[i] == 1){
@@ -549,7 +549,7 @@ std::cout << "File is " << fileList.at(f).c_str() << endl;
                         M++;
                     }
 
-                    if (jet_dau_eta>3.00 && jet_dau_eta<5.00){
+                    if (jet_dau_eta>2.25 && jet_dau_eta<5.00){
 
                         Q_all2T = Q_all2T + Q_part2;
                         Q_all4T = Q_all4T + Q_part4;
