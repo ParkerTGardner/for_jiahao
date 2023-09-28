1459d02026e71608ced0a9cfad07c9385f5cae9d
yanjiahao <jy123@rice.edu>
2023-09-28 13:16:24 -0500

    dijet data

--- HeavyIonAnalysis_dijet/TrackAnalysis/batch/pythia_batch_data_output/pythia_sub_copy.sh
+++ HeavyIonAnalysis_dijet/TrackAnalysis/batch/pythia_batch_data_output/pythia_sub_copy.sh
@@ -10,4 +10,4 @@ cd ~/CMSSW_10_6_4_patch1/src
 eval `scramv1 runtime -sh`
 cd ~/second_checkout/for_jiahao/HeavyIonAnalysis_dijet/TrackAnalysis/batch/
 echo PWD: $PWD
-../bin/PYTHIA_gen_di_copy.exe  ./pythia_lists/list_cor_$1 0 1
+../bin/PYTHIA_gen_di_copy.exe  ./all_data_list/list_cor_$1 0 1
