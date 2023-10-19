4b4adeb3388778b32ca7aba2593079426c7a7f72
yanjiahao <jy123@rice.edu>
2023-10-19 15:14:58 -0500

    off MC suitable jt and mult

--- HeavyIonAnalysis_dijet/TrackAnalysis/include/sherpa_constants_2.h
+++ HeavyIonAnalysis_dijet/TrackAnalysis/include/sherpa_constants_2.h
@@ -1,8 +1,8 @@
 const int   trackbin                    =6 ;
 const int   ktbin = 8;
 const int ptbin = 6;
-const float ptbinbounds_lo[ptbin] = {0.3,0.5,1.0,1.5,2.0,2.5};
-const float ptbinbounds_hi[ptbin] = {0.8,1.0,1.5,2.0,2.5,3.0};
+const float ptbinbounds_lo[ptbin] = {0.3,0.7,1.0,1.5,2.0,2.5};
+const float ptbinbounds_hi[ptbin] = {0.7,1.0,1.5,2.0,2.5,3.0};
 const int mm = 1000;
 const int   trackbinbounds[trackbin]         = {0 ,0 ,25,35,60,65};
 const int   trackbinboundsUpper[trackbin]    = {mm,25,35,60,65,mm};
